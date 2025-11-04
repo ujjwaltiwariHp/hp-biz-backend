8316df5de2b8f18d19abdf16b0eebe2721c9ca49
ujjwaltiwariHp <ujjwaltiwari.hp@gmail.com>
2025-11-04 11:18:21 +0530

    integrated invoice page

--- src/config/database.js
+++ src/config/database.js
@@ -2,17 +2,20 @@ const { Pool } = require('pg');
 require('dotenv').config();
 
 const pool = new Pool({
-    connectionString: process.env.DATABASE_URL,
-    ssl: process.env.NODE_ENV === 'production' ? { rejectUnauthorized: false } : false,
+  connectionString: process.env.DATABASE_URL,
+  ssl: {
+    require: true,
+    rejectUnauthorized: false,
+  },
 });
 
 pool.on('connect', () => {
-    console.log('Connected to PostgreSQL database');
+  console.log('Connected to PostgreSQL database');
 });
 
 pool.on('error', (err) => {
-    console.error('Unexpected PostgreSQL error:', err);
-    process.exit(-1);
+  console.error('Unexpected PostgreSQL error:', err);
+  process.exit(-1);
 });
 
 module.exports = pool;
