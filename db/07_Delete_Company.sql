b920b263579e0a061c35ef2f9d7ec190ddd4ce14
ujjwaltiwariHp <ujjwaltiwari.hp@gmail.com>
2025-11-25 17:12:06 +0530

    fixed error in sub-admin login

--- db/07_Delete_Company.sql
+++ db/07_Delete_Company.sql
@@ -1,8 +1,2 @@
-ALTER TABLE system_logs
-DROP CONSTRAINT IF EXISTS system_logs_company_id_fkey;
-
-ALTER TABLE system_logs
-ADD CONSTRAINT system_logs_company_id_fkey
-    FOREIGN KEY (company_id)
-    REFERENCES companies(id)
-    ON DELETE CASCADE;
+ALTER TABLE super_admin_sessions
+ALTER COLUMN refresh_token TYPE TEXT;
