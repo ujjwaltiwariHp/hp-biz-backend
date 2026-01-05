06ecd5d4073fd42c952913b1059d0efa2bcd75bb
ujjwaltiwariHp <ujjwaltiwari.hp@gmail.com>
2026-01-05 13:42:14 +0530

    update access token expiration time

--- src/utils/jwtHelper.js
+++ src/utils/jwtHelper.js
@@ -2,7 +2,7 @@ const jwt = require('jsonwebtoken');
 
 const generateAccessToken = (payload) => {
   return jwt.sign(payload, process.env.JWT_SECRET, {
-    expiresIn: '1h'
+    expiresIn: '24h'
   });
 };
 
